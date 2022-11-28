c259d432f99d7ce63a966a3f45c7028b31d8d1d2
Даниил <geimer_cok@mail.ru>
2022-11-28 22:05:00 +0300

    задача 3 готова

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+
+int interXY(string str)
+{
+    Console.Write(str);
+    int numberX = Convert.ToInt32(Console.ReadLine());
+    return numberX;
+}
+
+void findLength(int x1, int y1, int x2, int y2)
+{
+    double length = Math.Sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+    Console.WriteLine(length);
+}
+
+
+int x1 = interXY("Введите координату Х первой точки: ");
+int y1 = interXY("Введите координату Y первой точки: ");
+int x2 = interXY("Введите координату Х второй точки: ");
+int y2 = interXY("Введите координату Y второй точки: ");
+
+findLength(x1, y1, x2, y2);
--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Math.Sqrt Math.power(x2 - x1) + Math.power(y2 - y1);
