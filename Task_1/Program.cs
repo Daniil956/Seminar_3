e4f4104b0469f8e48cf5f92be461dd0535ad3333
Даниил <geimer_cok@mail.ru>
2022-11-28 21:18:23 +0300

    готово

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int DataEntryXY(string str)
+{
+    Console.Write(str);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void FindQvandrat(int x, int y)
+{
+    if ((x > 0)&&(y > 0)) Console.WriteLine ("Это первый квадрат");
+    if ((x < 0)&&(y > 0)) Console.WriteLine ("Это второй квадрат");
+    if ((x < 0)&&(y < 0)) Console.WriteLine ("Это третий квадрат");
+    if ((x > 0)&&(y < 0)) Console.WriteLine ("Это четвёртый квадрат");
+}
+
+
+
+
+
+
+int x = DataEntryXY("Введите координаты: x = ");
+//Console.WriteLine(x);
+int y = DataEntryXY("Введите координаты: y = ");
+//Console.WriteLine(y);
+FindQvandrat (x, y);
+
